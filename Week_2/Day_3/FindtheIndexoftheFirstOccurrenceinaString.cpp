815419682a082c456e10dd4a16a952f3fea7f0cb
Manmeet Singh Deol <deolmanmeetsingh13@gmail.com>
2026-06-11 11:53:28 +0530

    Solve Week 2, Day 3:Find the Index of the First Occurrence in a String compilation code and runtime logic

---
+++ Week_2/Day_3/FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        size_t pos = haystack.find(needle);
+        return (pos != string::npos) ? (int)pos : -1;
+    }
+};
